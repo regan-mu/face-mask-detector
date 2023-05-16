39337545b517d1d9a59796dd2b09649fd92e3407
regan-mu <regansomi@gmail.com>
2023-05-16 15:57:07 +0300

    facemask detector project

---
+++ Src/app.py
@@ -0,0 +1,95 @@
+import streamlit as st
+import numpy as np
+import cv2
+import os
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+
+st.set_page_config(
+    page_title="Facemask Detector",
+    page_icon="random",
+    initial_sidebar_state="expanded",
+    layout="wide"
+)
+
+
+def load_face_detector_model():
+    detector_path = "../Models/face_detector"
+    prototxt_path = os.path.sep.join(
+        [detector_path, "deploy.prototxt"])
+    weight_path = os.path.sep.join(
+        [detector_path, 'res10_300x300_ssd_iter_140000.caffemodel'])
+
+    net = cv2.dnn.readNet(prototxt_path, weight_path)
+
+    return net
+
+
+def prediction(image):
+    mask_prediction = None
+    image = cv2.imdecode(np.fromstring(image.read(), dtype='uint8'), cv2.IMREAD_COLOR)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    network = load_face_detector_model()
+    model = load_model("../Models/model.h5")
+    h, w = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300), (104.0, 177.0, 123.0))
+    network.setInput(blob)
+    detection = network.forward()
+    for i in range(0, detection.shape[2]):
+        confidence = detection[0, 0, i, 2]
+        if confidence > 0.5:
+            # Get the X and Y coordinates
+            box = detection[0, 0, i, 3:7]*np.array([h, w, h, w])
+            (startx, starty, endx, endy) = box.astype('int')
+
+            # Ensure the bounding boxes fall within the dimensions of the frame
+            startx, starty = (max(0, startx), max(0, starty))
+            endx, endy = (min(w-1, endx), min(h-1, endy))
+
+            # Extract the ROI of the image, convert it to RGB and resize it to 350x350 and preprocess it
+            face = image[starty:endy, startx:endx]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (350, 350))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+            face = np.expand_dims(face, axis=0)
+
+            with_mask, without_mask = model.predict(face)[0]
+
+            # determine the bounding box and label color
+            mask_prediction = 'Mask' if with_mask > without_mask else 'No mask'
+            color = (0, 255, 0) if mask_prediction == 'Mask' else (255, 0, 0)
+
+            # include probability in the label
+            label = '{} : {:.1f}%'.format(mask_prediction, max(with_mask, without_mask)*100)
+
+            # Add the label and bounding boxes
+            cv2.putText(image, label, (startx, starty-10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+            cv2.rectangle(image, (startx, starty), (endx, endy), color, 2)
+    return image, mask_prediction
+
+
+# Sidebar
+st.sidebar.header("Facemask Detector".upper())
+capture_method = st.sidebar.radio("Face Capture Method", ("Face Upload", "Camera Capture"))
+
+face_image = None
+if capture_method == "Face Upload":
+    face_image = st.sidebar.file_uploader("Upload Face Image", type=["png", "jpg", "svg", "jpeg"])
+elif capture_method == "Camera Capture":
+    face_image = st.sidebar.camera_input("Capture Face")
+
+st.sidebar.subheader("Source code [Github](https://github.com/regan-mu)")
+st.sidebar.write("Predict whether the face detected is wearing a mask or not.")
+
+# Main Page
+uploaded, predicted = st.columns(2)
+if face_image:
+    with uploaded:
+        st.image(face_image)
+        st.text("Original Image")
+    with predicted:
+        image_pred, label = prediction(face_image)
+        st.image(image_pred)
+        st.text(f"Prediction: {label}")
---
+++ readme.md
@@ -0,0 +1,45 @@
+# Facemask Detector
+
+Yes, I know Covid-19 days are now behind us (Thank God) but I built this project to showcase how to build image classification models with  Convolutional Neural Networks and Transfer Learning.
+
+The app is hosted on Streamlit at the [URL](https://www.tensorflow.org)
+
+# Libraries Used
+
+* [Tensorflow](https://www.tensorflow.org/)
+* [Numpy](https://numpy.org/)
+* [OpenCV](https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html)
+* [Streamlit](https://streamlit.io/)
+
+# Overview of the training Process
+
+* The model is trained on two classes of images, faces with mask and without mask.
+* For the pre-trained model (Transfer Learning), I used the VGG16.
+* Check out other pretrained models from [keras](https://keras.io/api/applications/).
+* I used the VGG16 model only for feature extraction from the images, then I added  fully connected layers for the actual image classification.
+* Trained the model over 20 epoch with an Early Stopping Callback which actual stopped the training at 15 epochs.
+* The best accuracy on the validation data was 96.66%.
+* It's worth noting that I didn't thoroughly test the model with more robust evaluation metrics like precision/recall, that something I hope to do later.
+
+# How the streamlit app works
+
+1. The user uploads or captures an image using their camera.
+2. The image is first preprocessed, making it equal in size with the images used in the training.
+3. The image then goes though a *res10* facial detector model that detects the face from the image.
+4. The detected facial image is taken though the model which generates a prediction.
+5. Post process the prediction to generate the prediction label.
+6. Create a bounding box for the image, then displays the image on the app with the label of the prediction.
+
+# Notebook
+
+# Model
+
+# Applications
+
+* The model can be embedded into sytems, example in high risk research lab to allow entry only to people with masks on.
+
+# Contribution
+
+* Further improvements can be made to the model, to increase the performance.
+* Maybe increase the training data or even a different pretrained model like resNet50.
+* Anyone interested in improving the model should feel free to fork it.
